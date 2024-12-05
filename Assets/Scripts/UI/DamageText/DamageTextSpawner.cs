1c8371b52056948066510ba289dec486f128d7ca
NhomToongTenz <155875453+NhomToongTenz@users.noreply.github.com>
2024-12-05 08:04:34 +0700

    init

---
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        public void Spawn(float damage)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+            instance.SetText(damage.ToString());
+        }
+    }
+}
